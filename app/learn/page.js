d4065ff9b52683ee5deaf07e499b8e9f13e6c628
Akash bauri <108834431+akashbauri@users.noreply.github.com>
2026-04-07 01:49:01 +0530

    Create page.js

---
+++ app/learn/page.js
@@ -0,0 +1,8 @@
+export default function Learn() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>Improve Your Loan Chances</h2>
+      <p>Learn saving, credit, and finance basics.</p>
+    </div>
+  );
+}
