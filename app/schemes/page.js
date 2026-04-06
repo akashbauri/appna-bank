9238ce956be6a35cb70cf981b9deff1abbc851a5
Akash bauri <108834431+akashbauri@users.noreply.github.com>
2026-04-07 01:49:46 +0530

    Create page.js

---
+++ app/schemes/page.js
@@ -0,0 +1,8 @@
+export default function Schemes() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>Free Benefits</h2>
+      <p>Explore government schemes and financial help.</p>
+    </div>
+  );
+}
